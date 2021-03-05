704a3a3208265cec3ef3adc1cd91a8608dc88945
Alexander <lebedevskiys@yandex.by>
2021-03-05 15:04:18 +0300

    First lab

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -93,6 +93,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_10);
+      HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
